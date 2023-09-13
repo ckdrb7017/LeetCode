b3430a045e778a429771d4a1631c982ba86f9942
ckdrb7017 <ckdrb7017@hanmail.net>
2023-09-13 18:04:42 +0900

    Time: 189 ms (12.98%), Space: 35.4 MB (43.40%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.kt
+++ 0088-merge-sorted-array/0088-merge-sorted-array.kt
@@ -1,10 +1,11 @@
 class Solution {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
-         val num1Size = nums1.size
-        nums2.forEachIndexed { index, i ->
-            nums1[num1Size - index - 1] = i
-        }
+        var i = m - 1
+        var j = n - 1
+        var k = m + n - 1
 
-        nums1.sort()
+        while (j >= 0){
+            nums1[k--] = if(i >= 0 && nums1[i] > nums2[j]) nums1[i--] else nums2[j--]
+        }       
     }
 }
