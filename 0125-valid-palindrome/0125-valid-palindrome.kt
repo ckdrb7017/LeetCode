a4899cd7accac729103dbcb46bb6afe33f06f756
ckdrb7017 <ckdrb7017@hanmail.net>
2023-09-13 18:47:43 +0900

    Time: 179 ms (79.37%), Space: 38.1 MB (33.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.kt
+++ 0125-valid-palindrome/0125-valid-palindrome.kt
@@ -1,17 +1,3 @@
 class Solution {
-    fun isPalindrome(s: String): Boolean {
-        val regex = "[a-zA-Z0-9]+"
-        val pattern = Pattern.compile(regex)
-        val matcher = pattern.matcher(s)
-        val sb = StringBuilder()
-        while (matcher.find()){
-            sb.append(matcher.group())
-        }
-        val str = sb.toString().lowercase()
-
-        for(i in 0 until str.length){
-            if(str[i] != str[str.length-i-1]) return false
-        }
-        return true
-    }
+    fun isPalindrome(s: String): Boolean  = s.filter { it.isLetterOrDigit() }.run { equals(reversed(), true) }
 }
